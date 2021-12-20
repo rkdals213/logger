45ad988b6510ae98954131eb883004114a845d6c
rkdals213 <rkdals213@naver.com>
2021-12-20 22:41:33 +0900

    git

--- src/main/kotlin/com/example/logger/app/service/Service.kt
+++ src/main/kotlin/com/example/logger/app/service/Service.kt
@@ -5,13 +5,16 @@ import com.example.logger.app.dto.TeamDto
 import com.example.logger.app.repository.MemberRepository
 import com.example.logger.app.repository.TeamRepository
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class Service(
     private val teamRepository: TeamRepository,
     private val memberRepository: MemberRepository
 ) {
+    @Transactional(readOnly = true)
     fun findAllTeam(): List<TeamDto> = teamRepository.findAll().map { TeamDto(it) }
 
+    @Transactional(readOnly = true)
     fun findAllMember(): List<MemberDto> = memberRepository.findAll().map { MemberDto(it) }
 }
