6176e9622a8d785472f54ea84ccfd879f312d1c8
rkdals213 <rkdals213@naver.com>
2021-12-24 17:04:05 +0900

    redis

--- src/main/kotlin/com/example/logger/app/dto/TeamDto.kt
+++ src/main/kotlin/com/example/logger/app/dto/TeamDto.kt
@@ -2,13 +2,12 @@ package com.example.logger.app.dto
 
 import com.example.logger.app.entity.Member
 import com.example.logger.app.entity.Team
-import java.io.Serializable
 
 data class TeamDto(
     var teamId: Long = 0L,
     var teamName: String = "",
     var members: List<MemberDto> = listOf()
-) : Serializable {
+) {
     constructor(team: Team) : this(
         team.id,
         team.name,
@@ -19,7 +18,7 @@ data class TeamDto(
 data class MemberDto(
     var memberId: Long = 0L,
     var memberName: String = ""
-) : Serializable {
+) {
     constructor(member: Member) : this(
         member.id,
         member.name
