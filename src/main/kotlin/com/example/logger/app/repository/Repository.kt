96a1bdade5a865a28b10bf7c9f66e367796b84a9
rkdals213 <rkdals213@naver.com>
2021-12-24 14:47:59 +0900

    redis

--- src/main/kotlin/com/example/logger/app/repository/Repository.kt
+++ src/main/kotlin/com/example/logger/app/repository/Repository.kt
@@ -6,8 +6,8 @@ import com.example.logger.app.redis.TeamDtoPoint
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
-interface TeamRepository: JpaRepository<Team, Long>
+interface TeamRepository : JpaRepository<Team, Long>
 
-interface MemberRepository: JpaRepository<Member, Long>
+interface MemberRepository : JpaRepository<Member, Long>
 
-interface TeamRedisRepository: CrudRepository<TeamDtoPoint, String>
+interface TeamRedisRepository : CrudRepository<TeamDtoPoint, String>
